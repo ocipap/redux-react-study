3e48d33e4cdd3af35a2b32dc65c1b630728a38e8
PAPICO <papico@inflearn.com>
2020-09-30 23:24:39 +0900

    feat: 액션 생성 함수 정의

--- src/stroe/modules/counter.js
+++ src/stroe/modules/counter.js
@@ -4,3 +4,9 @@ const CHANGE_COLOR = 'counter/CHANGE_COLOR';
 const INCREMENT = 'counter/INCREMENT';
 const DECREMENT = 'counter/DECREMENT';
 
+// 액션 생성 함수 정의
+export const changeColor = color => ({ type: CHANGE_COLOR, color });
+export const increment = () => ({ type: INCREMENT });
+export const decrement = () => ({ type: DECREMENT });
+
+
