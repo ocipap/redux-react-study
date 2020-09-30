a4b22fbd2a9e9ccac05430ea81de5a85cd25fe88
PAPICO <papico@inflearn.com>
2020-09-30 23:36:54 +0900

    feat: 스토어 생성 및 개발자 도구 적용

--- src/index.js
+++ src/index.js
@@ -1,8 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+
+import { createStore } from 'redux';
+import rootReducer from './store/modules';
+
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const devTools = window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__();
+const store = createStore(rootReducer, devTools);
+
+
+ReactDOM.render(<App/>, document.getElementById('root'));
 registerServiceWorker();
